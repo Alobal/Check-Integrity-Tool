047ca61ab07aa84bc204d45859a1550f3ecc31fe
Sitch <778377698@qq.com>
2022-02-13 14:22:56 +0800

    功能，增加检查功能的框架

--- MakeHash.py
+++ MakeHash.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 import os
+import sys
+import pickle #序列化保存
 import hashlib
+
 
 
 
@@ -13,41 +16,75 @@ class Node:
         self.value=value#hash值
 
     def AddChild(self,Node):
+        Node.parent=self
         self.child_list.append(Node)
 
     def __str__(self) -> str:#打印函数
-        return f'name: {self.name} || value: {self.value}'
+        return f'name: {self.name} || value: {self.value.hex()}'
 
 
 def Hash_file(path,hash=hashlib.md5()) -> bytearray : 
-    """计算单个文件的Hash值，返回Hash值的字节序列"""
-    value=None
+    """计算单个文件的Hash值，返回Hash值的Node"""
+    path=Path(path)
     with open(path,'rb') as f:
-        while True:
-            content=f.read(10240)#分块读取 避免大文件处理
+        while True:#分块读取并计算hash 避免大文件处理
+            content=f.read(10240)
             if not content:
                 break
             hash.update(content)
 
-    value=hash.digest()
-    return value
+    
+    node=Node(path.name,hash.digest())
 
-def Hash_Dir(root,hash=hashlib.md5()):
-    tree_root=Node(root)
+    return node
 
-    for root_path,dir_names,file_names in os.walk(root):#不包含子目录
-        
-        """获得所有目录的Hash值"""
-        for dir in dir_names:
-            dir_path=os.path.join(root_path,dir)
-            dir_hash=Hash_Dir(dir_path)
-            hash.update(dir_hash)
+def Hash_Dir(root,hash=hashlib.md5(),exclude_files=['.hash',os.path.basename(sys.argv[0])],flash=True) -> Node:
+    """计算一个目录的Hash值，并且构造Hash树"""
+    root_node=Node(root)
 
-        """获得所有当前目录文件的Hash值"""
-        for file in file_names:
+    root_path,dir_names,file_names=next(os.walk(root))#不包含子目录
+
+
+    """获得所有当前目录下的文件夹的Hash值"""
+    for dir in dir_names:
+        dir_path=os.path.join(root_path,dir)
+        dir_node=Hash_Dir(dir_path)
+        #添加子节点
+        root_node.AddChild(dir_node)
+        hash.update(dir_node.value)
+
+    """获得所有当前目录文件的Hash值"""
+    for file in file_names:
+        if file not in exclude_files:
             file_path=os.path.join(root_path,file)
-            file_hash=Hash_file(file_path)
-            hash.update(file_hash)
+            file_node=Hash_file(file_path)
+            #添加子节点
+            root_node.AddChild(file_node)
+            hash.update(file_node.value)
 
+    root_node.value=hash.digest()
 
-Hash_Dir("./")
+    if flash:#需要刷新保存的hash值
+        """在当前目录创建.hash文件，保存当前目录的hash节点值"""
+        with open(Path(root)/".hash",'w') as f:
+            f.write(root_node.value.hex())
+        
+        """序列化保存子树，便于检查文件尺度的错误"""
+        with open(Path(root)/".tree",'w') as f:
+            pickle.dump(root_node,f)
+
+    return root_node
+
+def Check_Dir(root,hash=hashlib.md5(),exclude_files=['.hash',os.path.basename(sys.argv[0])]):
+    """检查当前目录以及子目录的hash"""
+
+    """读取保存的hash值"""
+    with open(Path(root)/".hash",'rb') as f:
+        save_hash=f.read()
+    
+    """递归检查所有当前目录子目录的hash"""
+
+    """递归检查所有当前目录文件的hash"""
+
+if __name__== '__main__':
+    tree=Hash_Dir("./")
